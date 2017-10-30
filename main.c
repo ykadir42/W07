5709f112a159c833669236cb7e79095e1e4bb796
ykadir42 <ykadir42@gmail.com>
2017-10-30 00:26:51 -0400

    Made both arrays print random numbers

--- main.c
+++ main.c
@@ -8,13 +8,12 @@
 
 int random_gen(){
 
-	int fd = open("/dev/random", O_RDONLY | O_EXCL);
-	if(fd = -1){
+	int fd = open("/dev/random", O_RDONLY);
+	if(fd == -1){
 		printf("fd: %d\n", fd);
 		printf("errno: %d\n", errno);
 		printf("error: %s\n", strerror(errno));
 	}
-
 	int *buff = (int *)malloc(sizeof(int));
 	read(fd, buff, sizeof(int));
 	close(fd);
@@ -26,7 +25,25 @@ int random_gen(){
 int main(){
 	int random[10];
 	int i;
-	for(i = 0; i < 10; i++)
+	printf("random:\n");
+	for(i = 0; i < 10; i++){
 		random[i] = random_gen();
+		printf("\trandom[%d]: %d\n", i, random[i]);
+	}
+	int fd = open("array", O_CREAT | O_RDWR, 0644);
+	if(fd == -1){
+		printf("fd: %d\n", fd);
+		printf("errno: %d\n", errno);
+		printf("error: %s\n", strerror(errno));
+	}
+	write(fd, random, sizeof(random));
+	int random2[10];
+	read(fd, random2, sizeof(random2));
+	printf("random2:\n");
+	for(i = 0; i < 10; i++){
+		random[i] = random_gen();
+		printf("\trandom2[%d]: %d\n", i, random[i]);
+	}
+	close(fd);
 	return 0;
 }
